e3e62e5e8ae036efa9434bf0567307f7ddbafac4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-21 11:24:20 +0700

    1234

---
+++ ang_asp_workshop/frontend/src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// Material Buttons & Indicators
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatBadgeModule } from '@angular/material/badge';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+  ],
+})
+export class MaterialModule {}
