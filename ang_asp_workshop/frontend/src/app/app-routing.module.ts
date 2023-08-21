8105af83c9dda03ed95b93da0efd995f4c6d9064
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-21 14:56:10 +0700

    1234

--- ang_asp_workshop/frontend/src/app/app-routing.module.ts
+++ ang_asp_workshop/frontend/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: 'login' }, // undefined pages
 ];
 
 @NgModule({
