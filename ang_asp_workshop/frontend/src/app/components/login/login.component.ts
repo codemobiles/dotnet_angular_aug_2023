052ee70be3c490841d97b470fb1698e51f9dbdb4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-21 15:08:28 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/login/login.component.ts
+++ ang_asp_workshop/frontend/src/app/components/login/login.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
+  onSubmit(value: any) {
+    alert(JSON.stringify(value));
+  }
 }
