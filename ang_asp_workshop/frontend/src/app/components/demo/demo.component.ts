8d6c60296a90f722e29f8f4528bcdb79e7f8add9
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-21 11:53:20 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/demo/demo.component.ts
+++ ang_asp_workshop/frontend/src/app/components/demo/demo.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss'],
 })
-export class DemoComponent {}
+export class DemoComponent {
+  @Input() header: string = '';
+}
