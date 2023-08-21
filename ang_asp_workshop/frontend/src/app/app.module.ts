923097a21e86dbdb03048156a24bdc9319eaa2c8
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-21 11:45:39 +0700

    1234

--- ang_asp_workshop/frontend/src/app/app.module.ts
+++ ang_asp_workshop/frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { DemoComponent } from './components/demo/demo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DemoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
