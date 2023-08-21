a6ca5ae8e210c42facb7069e721a61458225ea88
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-21 15:53:52 +0700

    1234

--- ang_asp_workshop/frontend/src/app/services/rest.service.ts
+++ ang_asp_workshop/frontend/src/app/services/rest.service.ts
@@ -9,4 +9,9 @@ export class RestService {
   public assets(image: string): string {
     return `/assets/${image}`;
   }
+
+  public get isLoggedIn(): boolean {
+    // return localStorage.getItem(environment.token) != null;
+    return true;
+  }
 }
