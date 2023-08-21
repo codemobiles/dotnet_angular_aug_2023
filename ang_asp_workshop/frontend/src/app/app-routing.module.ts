0cd4d866ef744d2284d7f6a79a3035de0189016b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-21 14:54:28 +0700

    1234

--- ang_asp_workshop/frontend/src/app/app-routing.module.ts
+++ ang_asp_workshop/frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
