b6b2d6290318590eb086c9b34bfda5708e591fa5
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-21 15:25:15 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/login/login.component.ts
+++ ang_asp_workshop/frontend/src/app/components/login/login.component.ts
@@ -7,6 +7,8 @@ import { User } from 'src/app/models/user.model';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  bannerImage = 'assets/images/login_banner.jpg';
+
   onSubmit(value: User) {
     alert(JSON.stringify(value));
   }
