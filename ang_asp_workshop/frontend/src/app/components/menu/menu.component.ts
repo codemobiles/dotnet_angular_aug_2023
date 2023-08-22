4dd51e90391192db9defee4de18ec6fb2b971699
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-22 09:05:07 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/menu/menu.component.ts
+++ ang_asp_workshop/frontend/src/app/components/menu/menu.component.ts
@@ -1,10 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
-  @Input() isOpened = false;
+export class MenuComponent implements OnInit {
+  @Input() isOpened?: boolean;
+  @Output() sidenavToggle = new EventEmitter<void>();
+
+  constructor(public rest: RestService) {}
+
+  ngOnInit() {}
+
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
 }
