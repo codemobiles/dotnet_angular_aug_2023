0ac8e6c9e655003686d0310f196a04ce02e90be1
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-22 11:04:57 +0700

    1234

---
+++ ang_asp_workshop/backend/Controllers/v1/AuthController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using v1.Models;
+
+namespace backend.Controller.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+        }
+
+    }
+}
