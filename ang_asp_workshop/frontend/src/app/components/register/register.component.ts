e35f97dcc8dcb6b1bf4a2568be27d2dd8253a0c4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-22 09:42:59 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/register/register.component.ts
+++ ang_asp_workshop/frontend/src/app/components/register/register.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { RestService } from './../../services/rest.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { User } from 'src/app/models/user.model';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  hide = true;
 
+  constructor(private router: Router, public rest: RestService) {}
+
+  ngOnInit(): void {}
+
+  async onSubmit(value: User) {}
+
+  onClickCancel() {
+    this.router.navigate(['/login']);
+    // this.location.back();
+  }
 }
