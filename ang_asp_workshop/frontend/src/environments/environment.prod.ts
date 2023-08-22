1555e6bc8a6db31a51ef0a4763985e0b731f29fd
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-22 09:13:03 +0700

    1234

---
+++ ang_asp_workshop/frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  node_api_url: 'http://localhost:8081',
+  // node_api_url: 'http://localhost:8082',
+  node_static_url: 'http://localhost:8081',
+  baseUrl: '',
+  token: 'token',
+  version: '2.0',
+};
---
+++ ang_asp_workshop/frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  node_api_url: 'https://localhost:8081',
+  // node_api_url: 'http://localhost:8082',
+  node_static_url: 'https://localhost:8081',
+  baseUrl: '',
+  token: 'token',
+  version: '1.1 BETA',
+};
