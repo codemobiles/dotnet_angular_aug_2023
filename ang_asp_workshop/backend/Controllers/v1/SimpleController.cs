fe46fb168194c52a32a642dc76808ac4cf97e371
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-22 11:05:21 +0700

    1234

--- ang_asp_workshop/backend/Controllers/v1/SimpleController.cs
+++ ang_asp_workshop/backend/Controllers/v1/SimpleController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 //using backend.Models;
 
-namespace backend.Controllers
+namespace backend.Controllers.v1
 {
     [Route("api/[controller]")]
     [ApiController]
