4f9ca62fe3ed4673b9aae0c4f5758d1344520288
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-22 15:57:47 +0700

    1234

--- ang_asp_workshop/backend/Controllers/v1/AuthController.cs
+++ ang_asp_workshop/backend/Controllers/v1/AuthController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Models;
+using backend.Services;
 // using backend.Models;
 using Microsoft.AspNetCore.Mvc;
 //using v1.Models;
@@ -12,14 +14,18 @@ namespace backend.Controller.v1
     [Route("api/v1/[controller]")]
     public class AuthController : ControllerBase
     {
-        public AuthController()
+        private readonly IAuthRepository _authRepository;
+
+        public AuthController(IAuthRepository authRepository)
         {
+            _authRepository = authRepository;
         }
 
-        // [HttpPost("[action]")]
-        // public IActionResult Register([FromBody] User user)
-        // {
-        //     return Ok(new { result = "nok", message = "Register failed" });
-        // }
+        [HttpPost("[action]")]
+        public IActionResult Register([FromBody] User user)
+        {
+
+            return Ok(new { result = "nok", message = "Register failed" });
+        }
     }
 }
