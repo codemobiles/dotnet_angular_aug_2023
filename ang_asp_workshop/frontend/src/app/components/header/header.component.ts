a9fe6185634516bda5899f387b43f28c3bd378f4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-22 09:08:58 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/header/header.component.ts
+++ ang_asp_workshop/frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,36 @@
-import { Component, Input } from '@angular/core';
-
+import { RestService } from 'src/app/services/rest.service';
+import { environment } from 'src/environments/environment';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  @Input() isOpened = false;
+export class HeaderComponent implements OnInit {
+  @Output() sidenavToggle = new EventEmitter<void>();
+  @Input() isOpened?: boolean;
+  pageName: string = '';
+  version = environment.version;
+
+  constructor(private router: Router, public rest: RestService) {
+    // this.router.events
+    //   .filter((event) => event instanceof NavigationStart)
+    //   .pairwise()
+    //   .subscribe((event: any) => {
+    //     // alert(JSON.stringify(event[1].url));
+    //     this.pageName = event[1].url;
+    //   });
+  }
+
+  ngOnInit() {}
+
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
+
+  onClickSignout() {
+    localStorage.removeItem(environment.token);
+    this.router.navigate(['login']);
+  }
 }
