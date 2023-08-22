70bde4e98de9f8dfa11f4723d1f069f0e7a08819
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-22 10:29:19 +0700

    1234

---
+++ ang_asp_workshop/backend/Controllers/SimpleController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleController : ControllerBase
+    {
+        public SimpleController()
+        {
+        }
+
+        [HttpGet("")]
+        public ActionResult<IEnumerable<string>> Getstrings()
+        {
+            return new List<string> { "React", "Angular", "VueJS" };
+        }
+
+        [HttpGet("result1")]
+        public ActionResult<string> GetResult1()
+        {
+            return "I am result1";
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<string> GetstringById(int id)
+        {
+            return null;
+        }
+
+        [HttpPost("")]
+        public ActionResult<string> Poststring(string model)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Putstring(int id, string model)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<string> DeletestringById(int id)
+        {
+            return null;
+        }
+    }
+}
