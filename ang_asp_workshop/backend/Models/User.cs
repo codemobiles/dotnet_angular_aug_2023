9a851c72c2b84176dbed8282574f9c7bd4625179
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-22 11:16:17 +0700

    1234

--- ang_asp_workshop/backend/Models/User.cs
+++ ang_asp_workshop/backend/Models/User.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Position { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+}
