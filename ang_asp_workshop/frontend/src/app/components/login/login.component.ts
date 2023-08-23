529e34e93848c036a6789310fd8b8d216281861b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-23 09:44:00 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/login/login.component.ts
+++ ang_asp_workshop/frontend/src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { RestService } from 'src/app/services/rest.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -16,8 +17,16 @@ export class LoginComponent {
   error = null;
 
   async onSubmit(value: User) {
-    const response = await lastValueFrom(this.rest.login(value));
-    
+    let response = await lastValueFrom(this.rest.login(value));
+
+    if (response.token) {
+      this.error = null;
+      localStorage.setItem(environment.token, response.token);
+      this.router.navigate(['stock']);
+    } else {
+      this.error = response.message;
+      localStorage.removeItem(environment.token);
+    }
   }
 
   onClickRegister() {
