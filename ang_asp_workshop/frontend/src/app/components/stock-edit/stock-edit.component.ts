8754dae729ed3aaf77234e713a8ae9a29361bae3
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-23 13:46:40 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/stock-edit/stock-edit.component.ts
+++ ang_asp_workshop/frontend/src/app/components/stock-edit/stock-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-stock-edit',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-edit.component.scss'],
 })
 export class StockEditComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+
   ngOnInit(): void {
     const productId = this.route.snapshot.params['id'];
     alert(productId.toString());
