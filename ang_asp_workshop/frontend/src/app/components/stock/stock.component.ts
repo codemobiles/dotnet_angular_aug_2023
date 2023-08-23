1e976938a0447d51c8655c4002cd2aa6d0fd1b18
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-23 11:35:29 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/stock/stock.component.ts
+++ ang_asp_workshop/frontend/src/app/components/stock/stock.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent {
+export class StockComponent implements OnInit, AfterViewInit, OnDestroy {
+  products: any[] = [];
 
+  constructor(private rest: RestService) {}
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  async loadData() {
+    this.products = await lastValueFrom(this.rest.getProducts());
+  }
+
+  ngAfterViewInit(): void {}
+  ngOnDestroy(): void {}
 }
