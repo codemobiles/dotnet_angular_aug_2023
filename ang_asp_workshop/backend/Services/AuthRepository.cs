eba2fc18aeffecb8f7262a5e7f3e850ce18a7c93
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-23 09:19:16 +0700

    1234

--- ang_asp_workshop/backend/Services/AuthRepository.cs
+++ ang_asp_workshop/backend/Services/AuthRepository.cs
@@ -27,5 +27,45 @@ namespace backend.Services
             _context.Users.Add(user);
             _context.SaveChanges();
         }
+
+
+        private string CreatePasswordHash(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+            return $"{Convert.ToBase64String(salt)}.{hashed}";
+        }
+
+        public bool VerifyPassword(string hash, string password)
+        {
+            var parts = hash.Split('.', 2);
+
+            if (parts.Length != 2)
+            {
+                return false;
+            }
+
+            var salt = Convert.FromBase64String(parts[0]);
+            var passwordHash = parts[1];
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return passwordHash == hashed;
+        }
+
     }
 }
