de73140a2b54070787aef18f79439b8e2d03b5e7
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-23 11:11:42 +0700

    1234

---
+++ ang_asp_workshop/backend/Controllers/v1/ProductController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using Controllers.Models;
+
+namespace backend.Controller.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+        }
+
+    }
+}
