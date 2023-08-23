9bbb4ff4562fef16e33fb86b6ab94b4b2daa48f8
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-23 15:30:11 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/confirm-create-dialog/confirm-create-dialog.component.ts
+++ ang_asp_workshop/frontend/src/app/components/confirm-create-dialog/confirm-create-dialog.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RestService } from 'src/app/services/rest.service';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-confirm-create-dialog',
   templateUrl: './confirm-create-dialog.component.html',
-  styleUrls: ['./confirm-create-dialog.component.scss']
+  styleUrls: ['./confirm-create-dialog.component.scss'],
 })
 export class ConfirmCreateDialogComponent {
+  constructor(
+    public rest: RestService,
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      title: string;
+      subtitle: string;
+      item: any;
+    }
+  ) {}
 
+  ngOnInit(): void {}
+
+  onClickConfirm() {
+    this.dialogRef.close(true);
+  }
 }
