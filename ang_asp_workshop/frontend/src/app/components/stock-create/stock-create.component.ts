f7b7bbd7338564d7542ced47ed8d8e46a0b4239e
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-23 15:19:15 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/stock-create/stock-create.component.ts
+++ ang_asp_workshop/frontend/src/app/components/stock-create/stock-create.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { lastValueFrom } from 'rxjs';
 import { RestService } from 'src/app/services/rest.service';
 
 @Component({
@@ -14,11 +15,25 @@ export class StockCreateComponent implements OnInit {
   formProduct!: FormGroup;
   constructor(private rest: RestService, private location: Location) {}
 
-  onChangeImage($event: Event) {}
+  onChangeImage(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      // for preview
+      reader.onload = (event: any) => {
+        this.imageURL = event.target.result;
+      };
+
+      // for upload
+      reader.readAsDataURL(event.target.files[0]);
+      this.imageFile = event.target.files[0];
+    }
+  }
 
   ngOnInit(): void {
     this.initForm();
   }
+
   initForm() {
     this.formProduct = new FormGroup({
       name: new FormControl('Your Product', [
@@ -26,12 +41,22 @@ export class StockCreateComponent implements OnInit {
         Validators.maxLength(10),
       ]),
       stock: new FormControl(10, [Validators.required]),
-      price: new FormControl(200, []),
+      price: new FormControl(200, [Validators.required]),
+      image: new FormControl(null, [Validators.required]),
     });
   }
 
-  onSubmit() {
-    alert(JSON.stringify(this.formProduct.value));
+  async onSubmit() {
+    if (this.formProduct != null && this.imageFile != null) {
+      const formData = new FormData();
+      formData.append('name', this.formProduct.value.name);
+      formData.append('price', this.formProduct.value.price);
+      formData.append('stock', this.formProduct.value.stock);
+      formData.append('file', this.imageFile);
+
+      await lastValueFrom(this.rest.addProduct(formData));
+      this.location.back();
+    }
   }
 
   onClickCancel() {
