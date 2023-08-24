29647352e10fc4354a1c93b94dadc931bd85ba2b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-24 15:24:32 +0700

    1234

--- ang_asp_workshop/frontend/src/app/pipes/custom.pipe.ts
+++ ang_asp_workshop/frontend/src/app/pipes/custom.pipe.ts
@@ -6,7 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CustomPipe implements PipeTransform {
   transform(value: unknown, ...args: unknown[]): unknown {
     if (value) {
-      return `${value} ${args[0]}`;
+      return `${value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} ${
+        args[0]
+      }`;
     } else {
       return `0 บาท`;
     }
