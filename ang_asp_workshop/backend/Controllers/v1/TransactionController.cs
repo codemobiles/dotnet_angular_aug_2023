2517f2d0e5f938e58340f43bbcd88d461d89d4a3
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-24 15:29:01 +0700

    1234

---
+++ ang_asp_workshop/backend/Controllers/v1/TransactionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+//using backend.v1.Models;
+
+namespace backend.Controllers.v1
+{
+    [Authorize]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+
+        private readonly DatabaseContext databaseContext;
+
+        public TransactionController(DatabaseContext databaseContext)
+        {
+
+            this.databaseContext = databaseContext;
+        }
+
+    }
+}
