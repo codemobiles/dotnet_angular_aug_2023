4e0ef93502ec932106f9d4e65792ea99449233b8
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-24 14:19:47 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/payment/payment.component.ts
+++ ang_asp_workshop/frontend/src/app/components/payment/payment.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.scss']
+  styleUrls: ['./payment.component.scss'],
 })
 export class PaymentComponent {
-
+  @Output() onCompleted = new EventEmitter();
+  @Input() productOrder: any;
+  @Input() totalNumber: any;
 }
