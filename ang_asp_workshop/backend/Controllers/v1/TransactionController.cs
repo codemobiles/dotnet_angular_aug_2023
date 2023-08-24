6dacfdffd4572679c9a2c56d277379754b57a337
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-24 15:37:39 +0700

    1234

--- ang_asp_workshop/backend/Controllers/v1/TransactionController.cs
+++ ang_asp_workshop/backend/Controllers/v1/TransactionController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.Database;
+using backend.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 //using backend.v1.Models;
 
 namespace backend.Controllers.v1
 {
-    [Authorize]
+    // [Authorize]
     [Route("api/v1/[controller]")]
     [ApiController]
     public class TransactionController : ControllerBase
@@ -20,6 +22,19 @@ namespace backend.Controllers.v1
         public TransactionController(DatabaseContext databaseContext)
         {
             this.databaseContext = databaseContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetTransactions()
+        {
+            // var result = databaseContext.Transactions.OrderByDescending(p => p.Timestamp).ToList();
+            var result = databaseContext.Transactions
+            .FromSqlRaw("SELECT t.transaction_id, t.subtotal, t.discount, " +
+            "t.shipping_cost, t.tax_percent, t.total, t.paid, t.change," +
+            "t.order_list, t.payment_type, t.payment_detail, t.seller_id," +
+            "t.buyer_id, t.comment, t.timestamp, u.Username employee_id " +
+            "FROM Transactions t LEFT JOIN Users u ON t.employee_id = u.ID order by t.timestamp desc").ToList();
+            return Ok(result);
         }
 
     }
