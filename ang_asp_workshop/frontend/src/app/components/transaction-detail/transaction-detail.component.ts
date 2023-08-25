15a685642ea75ef43def0ccffc34c16eee4e5a64
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-25 10:05:16 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/transaction-detail/transaction-detail.component.ts
+++ ang_asp_workshop/frontend/src/app/components/transaction-detail/transaction-detail.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-transaction-detail',
   templateUrl: './transaction-detail.component.html',
   styleUrls: ['./transaction-detail.component.scss'],
 })
-export class TransactionDetailComponent {
-  load(selectedId: string) {}
+export class TransactionDetailComponent implements OnInit {
+  orderString = '[]';
+
+  ngOnInit(): void {}
+
+  load(orderJSONString: string) {}
 }
