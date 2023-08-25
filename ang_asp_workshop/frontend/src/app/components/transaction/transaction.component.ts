8a6a23287ead3dc755282d9b2fbaa216468f96b4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-08-25 09:46:58 +0700

    1234

--- ang_asp_workshop/frontend/src/app/components/transaction/transaction.component.ts
+++ ang_asp_workshop/frontend/src/app/components/transaction/transaction.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { TransactionDetailComponent } from '../transaction-detail/transaction-detail.component';
+import * as moment from 'moment';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
-  styleUrls: ['./transaction.component.scss']
+  styleUrls: ['./transaction.component.scss'],
 })
-export class TransactionComponent {
+export class TransactionComponent implements OnInit, AfterViewInit {
+  displayedColumns = [
+    'transactionId',
+    'timestamp',
+    'employeeId',
+    'total',
+    'paid',
+    'discount',
+    'orderList',
+  ];
+  dataSource = new MatTableDataSource<any>();
+  @ViewChild('detail', { static: false })
+  detailComponent!: TransactionDetailComponent;
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
 
+  selectedId!: string;
+
+  constructor(private rest: RestService) {}
+
+  async ngOnInit() {
+    this.rest.getTransaction().subscribe((result) => {
+      this.dataSource.data = result;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  getNumberOfSKU(rawOrder: any): number {
+    let order = JSON.parse(rawOrder);
+    return order.length;
+  }
+
+  onClickRow(id: string) {
+    this.selectedId = id;
+    setTimeout(() => {
+      this.detailComponent.load(this.selectedId);
+    }, 100);
+  }
 }
